5366859e94dee337d3f37eed40b41812791b422a
notrich22 <notrich22@mail.ru>
2022-11-02 20:09:41 +0100

    ASP Logger added

--- JSONConvertationAPI/Program.cs
+++ JSONConvertationAPI/Program.cs
@@ -1,9 +1,11 @@
 using JSONConvertationAPI;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", async (context) => {
+    app.Logger.LogInformation($"Path: /  Time: {DateTime.Now.ToLongTimeString()}");
     Messages.StatusMessage responseData =
         new Messages.StatusMessage("Service is avaliable", DateTime.Now);
     Console.WriteLine(responseData);
@@ -11,23 +13,26 @@ app.MapGet("/", async (context) => {
 });
 app.MapGet("/ping", async (context) =>
 {
+    app.Logger.LogInformation($"Path: /ping  Time: {DateTime.Now.ToLongTimeString()}");
     Messages.StatusMessage status = new Messages.StatusMessage("pong", DateTime.Now);
     Console.WriteLine(status);
     await context.Response.WriteAsJsonAsync(status);
 });
 app.MapPost("/calculate", async (context) =>
 {
+    app.Logger.LogInformation($"Path: /calculate  Time: {DateTime.Now.ToLongTimeString()}");
     Messages.CalcInputMessage? convertation = null;
     try
     {
         convertation = await context.Request.ReadFromJsonAsync<Messages.CalcInputMessage>();
-        Console.WriteLine(convertation);
     }
     catch(Exception e) {
+        app.Logger.LogError($"Exception occured: {e.Message}: DateTime:{DateTime.Now.ToLongTimeString()}");
         Messages.ErrorMessage error = new Messages.ErrorMessage($"Error during request processing: {e.Message}");
     }
     if (convertation is null)
     {
+        app.Logger.LogError($"Exception occured: Convertation is null: DateTime:{DateTime.Now.ToLongTimeString()}");
         Messages.ErrorMessage error = new Messages.ErrorMessage("Invalid request parameters");
         Console.WriteLine(error);
         await context.Response.WriteAsJsonAsync(error);
@@ -40,9 +45,10 @@ app.MapPost("/calculate", async (context) =>
             Console.WriteLine(solvation);
             await context.Response.WriteAsJsonAsync(solvation);
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-            Messages.ErrorMessage error = new Messages.ErrorMessage($"Error during request processing: {ex.Message}");
+            app.Logger.LogError($"Exception occured: {e.Message}: DateTime:{DateTime.Now.ToLongTimeString()}");
+            Messages.ErrorMessage error = new Messages.ErrorMessage($"Error during request processing: {e.Message}");
             Console.WriteLine(error);
             await context.Response.WriteAsJsonAsync(error);
         }
